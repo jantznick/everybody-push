80178f9206a77b991b32b152129ce1f618fa91ef
Nick Jantz <thenickjantz@gmail.com>
2024-03-18 12:45:28 -0500

    frontend route management

--- app/routes/index.js
+++ app/routes/index.js
@@ -7,7 +7,14 @@ import { NotFound } from '../components/pages/NotFound.js';
 const AppRoutes = () => {
 	return (
         <Routes>
+            {/* landing page */}
             <Route path="/" element={<App/>} />
+            {/* app */}
+            <Route path="/app" element={<App/>} />
+            {/* users self settings: */}
+            <Route path="/settings" element={<Settings/>} />
+            {/* admin user and project management */}
+            <Route path="/admin-dashboard" element={<AdminDashboard/>} />
             <Route path="/404" element={<NotFound/>} />
             <Route path="/*" element={<NotFound/>} />
         </Routes>
