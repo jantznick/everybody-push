22539e9ef8749d5089240656907b546682015ac1
Nick Jantz <thenickjantz@gmail.com>
2024-03-25 21:43:07 -0500

    work on DB

--- api/models/index.js
+++ api/models/index.js
@@ -9,94 +9,135 @@ const sequelize = new Sequelize(process.env.DATABASE_URL, {
 	dialectModule: pg
 })
 
-const Org = sequelize.define('org', {
-	id: { type: Sequelize.INTEGER, primaryKey: true, autoIncrement: true },
-	name: Sequelize.STRING,
-	folder: Sequelize.STRING,
-	admin: Sequelize.STRING
-});
-
-const Team = sequelize.define('team', {
-	id: { type: Sequelize.INTEGER, primaryKey: true, autoIncrement: true },
-	name: Sequelize.STRING,
-	org: { type: Sequelize.INTEGER, references: { model: Org, key: 'id' } },
-	folder: Sequelize.STRING,
-	admin: Sequelize.STRING
-});
-
-const Project = sequelize.define('project', {
-	id: { type: Sequelize.INTEGER, primaryKey: true, autoIncrement: true },
-	name: Sequelize.STRING,
-	team: { type: Sequelize.INTEGER, references: { model: Team, key: 'id' } },
-	folder: Sequelize.STRING,
-	admin: Sequelize.STRING
-});
-
-const Category = sequelize.define('category', {
-	id: { type: Sequelize.INTEGER, primaryKey: true },
-	name: Sequelize.STRING,
-	project: { type: Sequelize.INTEGER, references: { model: Project, key: 'id' } },
-	admin: Sequelize.STRING
-});
-
 const User = sequelize.define('user', {
 	id: { type: Sequelize.STRING, primaryKey: true },
-	name: Sequelize.STRING,
+	firstName: Sequelize.STRING,
+	lastName: Sequelize.STRING,
 	email: Sequelize.STRING,
 	password: Sequelize.STRING,
 	createdAt: Sequelize.DATE,
 	lastActiveAt: Sequelize.DATE,
 	subscriptionTier: Sequelize.STRING,
 	subscriptionStatus: Sequelize.STRING,
-	org: Sequelize.STRING,
-	team: Sequelize.STRING,
-	project: Sequelize.STRING
+	org: Sequelize.ARRAY({ type: Sequelize.STRING,}),
+	team: Sequelize.ARRAY({ type: Sequelize.STRING }),
+	project: Sequelize.ARRAY({ type: Sequelize.STRING})
 }, {
 	hooks: {
 		beforeCreate: (user) => {
 			const salt = bcrypt.genSaltSync(10);
 			user.password = bcrypt.hashSync(user.password, salt);
 			user.id = uuidv4();
-			user.subscriptionTier = 'free'
-			user.subscriptionStatus = 'not-paid'
-			user.org = 'none'
-			user.team = 'none'
-			user.project = 'none'
+			user.subscriptionTier = user.subscriptionTier || 'free'
+			user.subscriptionStatus = user.subscriptionStatus || 'not-paid'
+			user.org = user.org || []
+			user.team = user.team || []
+			user.project = user.project || []
 		}
 	}
 });
 
+const Org = sequelize.define('org', {
+	id: { type: Sequelize.STRING, primaryKey: true },
+	name: Sequelize.STRING,
+	folder: Sequelize.STRING,
+	admin: Sequelize.ARRAY({ type: Sequelize.STRING, references: {model: User.id}})
+}, {
+	hooks: {
+		beforeCreate: (org) => {
+			org.id = uuidv4();
+			org.name = org.name || ''
+			org.folder = org.folder || ''
+			org.admin = org.admin || []
+		}
+	}
+});
+
+const Team = sequelize.define('team', {
+	id: { type: Sequelize.STRING, primaryKey: true },
+	name: Sequelize.STRING,
+	org: { type: Sequelize.STRING, references: { model: Org, key: 'id' } },
+	folder: Sequelize.STRING,
+	admin: Sequelize.ARRAY({ type: Sequelize.STRING, references: {model: User.id}})
+}, {
+	hooks: {
+		beforeCreate: (team) => {
+			team.id = uuidv4();
+			team.name = team.name || ''
+			team.org = team.org || ''
+			team.folder = team.folder || ''
+			team.admin = team.admin || []
+		}
+	}
+});
+
+const Project = sequelize.define('project', {
+	id: { type: Sequelize.STRING, primaryKey: true, autoIncrement: true },
+	name: Sequelize.STRING,
+	team: { type: Sequelize.STRING, references: { model: Team, key: 'id' } },
+	folder: Sequelize.STRING,
+	admin: Sequelize.ARRAY({ type: Sequelize.STRING, references: {model: User.id}})
+});
+
 const Task = sequelize.define('task', {
-	id: { type: Sequelize.INTEGER, primaryKey: true, autoIncrement: true },
+	id: { type: Sequelize.STRING, primaryKey: true, autoIncrement: true },
 	created_at: Sequelize.DATE,
+	created_by: { type: Sequelize.STRING, references: { model: User, key: 'id' } },
 	assignee: { type: Sequelize.STRING, references: { model: User, key: 'id' } },
 	done: Sequelize.BOOLEAN,
-	category: { type: Sequelize.INTEGER, references: { model: Category, key: 'id' } },
+	project: { type: Sequelize.INTEGER, references: { model: Project, key: 'id' } },
 	title: Sequelize.STRING,
+	status: Sequelize.STRING,
 	description: Sequelize.STRING,
 	createdAt: Sequelize.DATE,
 	dueDate: Sequelize.DATE
+}, {
+	hooks: {
+		beforeCreate: (task) => {
+			task.id = uuidv4();
+			var d = new Date(0);
+			d.setUTCSeconds(Date.now()/1000);
+			task.created_at = d
+			task.created_by = task.userId || 0
+			task.assignee = task.assignee || 0
+			task.done = task.done || false
+			task.project = task.project || []
+			task.title = task.title || ''
+			task.status = task.status || ''
+			task.description = task.description || ''
+			task.createdAt = d
+			task.dueDate = task.dueDate || null
+		}
+	}
+});
+
+const Category = sequelize.define('category', {
+	id: { type: Sequelize.INTEGER, primaryKey: true },
+	name: Sequelize.STRING,
+	project: { type: Sequelize.INTEGER, references: { model: Project, key: 'id' } },
+	admin: Sequelize.ARRAY({ type: Sequelize.STRING, references: {model: User.id}}),
+	tasks: Sequelize.ARRAY({ type: Sequelize.STRING, references: {model: Task.id}})
 });
 
 const Comment = sequelize.define('comment', {
-	id: { type: Sequelize.INTEGER, primaryKey: true, autoIncrement: true },
+	id: { type: Sequelize.STRING, primaryKey: true, autoIncrement: true },
 	name: Sequelize.STRING,
-	task: { type: Sequelize.INTEGER, references: { model: Task, key: 'id' } },
+	task: { type: Sequelize.STRING, references: { model: Task, key: 'id' } },
 	user: { type: Sequelize.STRING, references: { model: User, key: 'id' } },
 	content: Sequelize.STRING,
 	createdAt: Sequelize.DATE
 });
 
 const Tag = sequelize.define('tag', {
-	id: { type: Sequelize.INTEGER, primaryKey: true, autoIncrement: true },
+	id: { type: Sequelize.STRING, primaryKey: true, autoIncrement: true },
 	name: Sequelize.STRING,
-	task: { type: Sequelize.INTEGER, references: { model: Task, key: 'id' } }
+	task: { type: Sequelize.STRING, references: { model: Task, key: 'id' } }
 });
 
 const Subtask = sequelize.define('subtask', {
-	id: { type: Sequelize.INTEGER, primaryKey: true, autoIncrement: true },
+	id: { type: Sequelize.STRING, primaryKey: true, autoIncrement: true },
 	done: Sequelize.BOOLEAN,
-	task: { type: Sequelize.INTEGER, references: { model: Task, key: 'id' } },
+	task: { type: Sequelize.STRING, references: { model: Task, key: 'id' } },
 	assignee: { type: Sequelize.STRING, references: { model: User, key: 'id' } }
 });
 
@@ -121,8 +162,8 @@ const Session = sequelize.define('session', {
 	userId: { type: Sequelize.STRING, allowNull: false, references: { model: User, key: 'id' }}
 });
 
-sequelize.sync()
-.then(() => console.log('Tables have been successfully created, if one doesn\'t exist'))
+sequelize.sync({ alter: true })
+.then(() => console.log('Tables have been successfully updated, if one doesn\'t exist'))
 .catch(error => console.log('An error occurred while creating the table:', error));
 
 module.exports = {
---
+++ api/models/seedDB.js
@@ -0,0 +1,280 @@
+const {
+	sequelize,
+	Org,
+	Team,
+	Project,
+	Category,
+	Task,
+	Subtask,
+	Comment,
+	Tag,
+	User,
+	LoginToken,
+	ResetPasswordToken,
+	Session
+} = require('./index');
+const { v4 } = require('uuid');
+
+const users = [
+    {
+        id: v4(),
+        name: 'JakeWilliams',
+        email: 'jake@email.com',
+        password: 'password',
+        createdAt: '2020-09-19T03:24:00',
+        lastActiveAt: '2021-02-02T13:24:00',
+        subscriptionTier: 'Premium',
+        subscriptionStatus: 'Active',
+        org: [],
+        team: [],
+        project: []
+    }, {
+        id: v4(),
+        name: 'SamBrown',
+        email: 'sam@email.com',
+        password: 'password',
+        createdAt: '2021-01-01T11:30:00',
+        lastActiveAt: '2021-03-01T16:40:00',
+        subscriptionTier: 'Standard',
+        subscriptionStatus: 'Active',
+        org: [],
+        team: [],
+        project: []
+    }
+]
+
+const seedUsers = async () => {
+    console.log('seed users');
+    await User.bulkCreate(users, {validate: true})
+}
+seedUsers()
+
+const orgs = [
+    {
+        id: v4(),
+        name: 'Tech Titan Corporation',
+        folder: 'tech-titan',
+        admin: []
+    }, {
+        id: v4(),
+        name: 'Digital Dynamo Inc.',
+        folder: 'digital-dynamo',
+        admin: []
+    }
+]
+
+const seedOrgs = async () => {
+    return await Org.bulkCreate(orgs, {validate: true});
+}
+const [techTitan, digitalDynamo] = seedOrgs()
+
+const teams = [
+    {
+        id: v4(),
+        name: 'Mobile Development',
+        org: techTitan.id,
+        folder: 'mobile-dev',
+        admin: []
+    }, {
+        id: v4(),
+        name: 'CyberSecurity',
+        org: techTitan.id,
+        folder: 'cyber-sec',
+        admin: []
+    }, {
+        id: v4(),
+        name: 'AI Research',
+        org: techTitan.id,
+        folder: 'ai-research',
+        admin: []
+    }, {
+        id: v4(),
+        name: 'Cloud Solutions',
+        org: digitalDynamo.id,
+        folder: 'cloud-solutions',
+        admin: []
+    }, {
+        id: v4(),
+        name: 'Web Development',
+        org: digitalDynamo.id,
+        folder: 'web-dev',
+        admin: []
+    }
+]
+
+const seedTeams = async () => {
+    return await Team.bulkCreate(teams, {validate: true})
+}
+
+const [mobileDev, cyberSec, aiResearch, cloudSolutions, webDev] = seedTeams()
+const projects = [
+    {
+        id: v4(),
+        name: 'Customer Chatbot',
+        team: mobileDev.id,
+        folder: 'customer-chatbot',
+        admin: []
+    }, {
+        id: v4(),
+        name: 'Interactive Fleet Management App',
+        team: mobileDev.id,
+        folder: 'fleet-management',
+        admin: []
+    }, {
+        id: v4(),
+        name: 'Mobile Health Tracker',
+        team: mobileDev.id,
+        folder: 'health-tracker',
+        admin: []
+    }, {
+        id: v4(),
+        name: 'Secure Cloud Storage',
+        team: cyberSec.id,
+        folder: 'secure-cloud',
+        admin: []
+    }, {
+        id: v4(),
+        name: 'Intrusion Detection System',
+        team: cyberSec.id,
+        folder: 'ids',
+        admin: []
+    }, {
+        id: v4(),
+        name: 'Multi-Factor Authentication',
+        team: cyberSec.id,
+        folder: 'mfa',
+        admin: []
+    }, {
+        id: v4(),
+        name: 'Natural Language Processing',
+        team: aiResearch.id,
+        folder: 'nlp',
+        admin: []
+    }, {
+        id: v4(),
+        name: 'Predictive Customer Analytics',
+        team: aiResearch.id,
+        folder: 'customer-analytics',
+        admin: []
+    }, {
+        id: v4(),
+        name: 'Cloud Migration Strategy',
+        team: cloudSolutions.id,
+        folder: 'cloud-migration',
+        admin: []
+    }, {
+        id: v4(),
+        name: 'Cloud Backup & Restoration',
+        team: cloudSolutions.id,
+        folder: 'cloud-backup',
+        admin: []
+    }, {
+        id: v4(),
+        name: 'Software as a Service Platform',
+        team: cloudSolutions.id,
+        folder: 'saas-platform',
+        admin: []
+    }, {
+        id: v4(),
+        name: 'Website Redesign',
+        team: webDev.id,
+        folder: 'website-redesign',
+        admin: []
+    }, {
+        id: v4(),
+        name: 'E-commerce Platform',
+        team: webDev.id,
+        folder: 'ecommerce-platform',
+        admin: []
+    }, {
+        id: v4(),
+        name: 'Online Booking System',
+        team: webDev.id,
+        folder: 'online-booking',
+        admin: []
+    }, {
+        id: v4(),
+        name: 'Corporate Intranet Portal',
+        team: webDev.id,
+        folder: 'corporate-intranet',
+        admin: []
+    }
+]
+
+const seedProjects = async () => {
+    return await Project.bulkCreate(projects, {validate: true})
+}
+
+const [
+    customerChatbot,
+    fleetManagement,
+    healthTracker,
+    cloudStorage,
+    iDS,
+    mFA,
+    nLP,
+    customerAnalytics,
+    cloudMigration,
+    cloudBackup,
+    saasPlatform,
+    websiteRedesign,
+    ecommerce,
+    onlineBooking,
+    intranet
+] = seedProjects()
+
+const categories = [
+    {
+        id: v4(),
+        name: 'Frontend',
+        project: 1,
+        admin: [],
+        tasks: []
+    }, {
+        id: v4(),
+        name: 'Backend',
+        project: 1,
+        tasks: []
+    }, {
+        id: v4(),
+        name: 'UI Design',
+        project: 2,
+        tasks: []
+    }, {
+        id: v4(),
+        name: 'Server Setup',
+        project: 2,
+        tasks: []
+    }
+]
+
+const tasks = [
+    {
+        id: v4(),
+        created_at: '2020-10-01T02:00:00',
+        assignee: [],
+        done: false,
+        title: 'Create Bot Dialogue Tree',
+        status: 'in-progress',
+        description: 'Create a comprehensive dialogue tree for the customer chatbot',
+        createdAt: '2020-10-01T02:00:00',
+        dueDate: '2020-10-30T23:59:59',
+        project: customerChatbot.id
+    }, {
+        id: v4(),
+        created_at: '2020-11-01T02:00:00',
+        assignee: [],
+        done: false,
+        title: 'Set Up Training Model',
+        status: 'refinement',
+        description: 'Set up a model for the chatbot to learn from.',
+        createdAt: '2020-11-01T02:00:00',
+        dueDate: '2020-12-31T23:59:59',
+        project: customerChatbot.id
+    }
+]
+
+const seedTasks = async () => {
+    return await Task.bulkCreate(tasks, {validate: true})
+}
+seedTasks()
