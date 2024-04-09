1f3bee274f751aa3390eadfe5ecd160900e1e37c
Nick Jantz <thenickjantz@gmail.com>
2024-04-08 23:12:15 -0500

    start work on middleware

--- api/models/index.js
+++ api/models/index.js
@@ -11,7 +11,7 @@ const sequelize = new Sequelize(process.env.DATABASE_URL, {
 
 const getCurrentTimeStamp = () => {
 	var d = new Date(0);
-	d.setUTCSeconds(Date.now()/1000);
+	d.setUTCSeconds(Date.now() / 1000);
 	return d;
 }
 
@@ -54,9 +54,9 @@ const User = sequelize.define('user', {
 	last_active_at: Sequelize.DATE,
 	subscription_tier: Sequelize.STRING,
 	subscription_status: Sequelize.STRING,
-	org: Sequelize.ARRAY({ type: Sequelize.STRING,}),
+	org: Sequelize.ARRAY({ type: Sequelize.STRING, }),
 	team: Sequelize.ARRAY({ type: Sequelize.STRING }),
-	project: Sequelize.ARRAY({ type: Sequelize.STRING})
+	project: Sequelize.ARRAY({ type: Sequelize.STRING })
 }, {
 	hooks: {
 		beforeCreate: (user) => {
@@ -77,7 +77,7 @@ const Org = sequelize.define('org', {
 	id: { type: Sequelize.STRING, primaryKey: true },
 	name: Sequelize.STRING,
 	folder: Sequelize.STRING,
-	admin: Sequelize.ARRAY({ type: Sequelize.STRING, references: {model: User.id}})
+	admin: Sequelize.ARRAY({ type: Sequelize.STRING, references: { model: User.id } })
 }, {
 	hooks: {
 		beforeCreate: (org) => {
@@ -94,7 +94,7 @@ const Team = sequelize.define('team', {
 	name: Sequelize.STRING,
 	org: { type: Sequelize.STRING, references: { model: Org, key: 'id' } },
 	folder: Sequelize.STRING,
-	admin: Sequelize.ARRAY({ type: Sequelize.STRING, references: {model: User.id}})
+	admin: Sequelize.ARRAY({ type: Sequelize.STRING, references: { model: User.id } })
 }, {
 	hooks: {
 		beforeCreate: (team) => {
@@ -108,11 +108,11 @@ const Team = sequelize.define('team', {
 });
 
 const Project = sequelize.define('project', {
-	id: { type: Sequelize.STRING, primaryKey: true, autoIncrement: true },
+	id: { type: Sequelize.STRING, primaryKey: true },
 	name: Sequelize.STRING,
 	team: { type: Sequelize.STRING, references: { model: Team, key: 'id' } },
 	folder: Sequelize.STRING,
-	admin: Sequelize.ARRAY({ type: Sequelize.STRING, references: {model: User.id}})
+	admin: Sequelize.ARRAY({ type: Sequelize.STRING, references: { model: User.id } })
 }, {
 	hooks: {
 		beforeCreate: (project) => {
@@ -126,12 +126,12 @@ const Project = sequelize.define('project', {
 });
 
 const Task = sequelize.define('task', {
-	id: { type: Sequelize.STRING, primaryKey: true, autoIncrement: true },
+	id: { type: Sequelize.STRING, primaryKey: true },
 	created_at: Sequelize.DATE,
 	created_by: { type: Sequelize.STRING, references: { model: User, key: 'id' } },
 	assignee: { type: Sequelize.STRING, references: { model: User, key: 'id' } },
 	done: Sequelize.BOOLEAN,
-	project: { type: Sequelize.INTEGER, references: { model: Project, key: 'id' } },
+	project: { type: Sequelize.STRING, references: { model: Project, key: 'id' } },
 	title: Sequelize.STRING,
 	status: Sequelize.STRING,
 	description: Sequelize.STRING,
@@ -154,15 +154,15 @@ const Task = sequelize.define('task', {
 });
 
 const Category = sequelize.define('category', {
-	id: { type: Sequelize.INTEGER, primaryKey: true },
+	id: { type: Sequelize.STRING, primaryKey: true },
 	name: Sequelize.STRING,
-	project: { type: Sequelize.INTEGER, references: { model: Project, key: 'id' } },
-	admin: Sequelize.ARRAY({ type: Sequelize.STRING, references: {model: User.id}}),
-	tasks: Sequelize.ARRAY({ type: Sequelize.STRING, references: {model: Task.id}})
+	project: { type: Sequelize.STRING, references: { model: Project, key: 'id' } },
+	admin: Sequelize.ARRAY({ type: Sequelize.STRING, references: { model: User.id } }),
+	tasks: Sequelize.ARRAY({ type: Sequelize.STRING, references: { model: Task.id } })
 });
 
 const Comment = sequelize.define('comment', {
-	id: { type: Sequelize.STRING, primaryKey: true, autoIncrement: true },
+	id: { type: Sequelize.STRING, primaryKey: true },
 	name: Sequelize.STRING,
 	task: { type: Sequelize.STRING, references: { model: Task, key: 'id' } },
 	user: { type: Sequelize.STRING, references: { model: User, key: 'id' } },
@@ -171,13 +171,13 @@ const Comment = sequelize.define('comment', {
 });
 
 const Tag = sequelize.define('tag', {
-	id: { type: Sequelize.STRING, primaryKey: true, autoIncrement: true },
+	id: { type: Sequelize.STRING, primaryKey: true },
 	name: Sequelize.STRING,
 	task: { type: Sequelize.STRING, references: { model: Task, key: 'id' } }
 });
 
 const Subtask = sequelize.define('subtask', {
-	id: { type: Sequelize.STRING, primaryKey: true, autoIncrement: true },
+	id: { type: Sequelize.STRING, primaryKey: true },
 	done: Sequelize.BOOLEAN,
 	task: { type: Sequelize.STRING, references: { model: Task, key: 'id' } },
 	assignee: { type: Sequelize.STRING, references: { model: User, key: 'id' } }
@@ -201,12 +201,12 @@ const ResetPasswordToken = sequelize.define('resetPasswordToken', {
 
 const Session = sequelize.define('session', {
 	token: { type: Sequelize.STRING, allowNull: false },
-	user_id: { type: Sequelize.STRING, allowNull: false, references: { model: User, key: 'id' }}
+	user_id: { type: Sequelize.STRING, allowNull: false, references: { model: User, key: 'id' } }
 });
 
 sequelize.sync({ alter: true })
-.then(() => console.log('Tables have been successfully updated, if one doesn\'t exist'))
-.catch(error => console.log('An error occurred while creating the table:', error));
+	.then(() => console.log('Tables have been successfully updated, if one doesn\'t exist'))
+	.catch(error => console.log('An error occurred while creating the table:', error));
 
 module.exports = {
 	sequelize,
--- api/utils/authMiddleware.js
+++ api/utils/authMiddleware.js
@@ -1,30 +1,45 @@
+// const { 
+//     list,
+//     Sublist,
+//     Comment,
+//     Tag
+// } = require('../models');
+
+const {
+	Session,
+	User,
+	Task
+} = require('../models')
+
 export const isAuthorizedForThat = async (req, res, next) => {
     console.log("Is authorized for that Middleware:");
     console.log(req.cookies);
-	// try {
-	// 	const token = req.cookies.sessionId;
-	// 	if (!token) throw new Error("No token provided");
+	try {
+		const token = req.cookies.session_id;
+		if (!token) throw new Error("No token provided");
 
-	// 	const session = await Session.findOne({ where: { token: token } });
-	// 	if (!session) throw new Error("Session not found");
+		const session = await Session.findOne({ where: { token: token } });
+		if (!session) throw new Error("Session not found");
 
-	// 	const user = await User.findOne({ where: { id: session.user_id } });
-	// 	if (!user) throw new Error("User not found");
+		const user = await User.findOne({ where: { id: session.user_id } });
+		if (!user) throw new Error("User not found");
 
-	// 	// Check if user has the necessary permissions
-	// 	// As an example, check if user belongs to the same org
-	// 	const taskId = req.params.taskId || req.body.taskId; // depends on your endpoint
-	// 	const task = await Task.findOne({ where: { id: taskId } });
+		// Check if user has the necessary permissions
+		// As an example, check if user belongs to the same org
+		const taskId = req.params.taskId || req.body.taskId || 'zxcviu-234234-szdcxv-234-dv'; // depends on your endpoint
+		const task = await Task.findOne({ where: { id: taskId } });
 
-	// 	// this is half sudo code to be fixed
-	// 	if (!user.project.contains(task.org) && !user.team.contains(task.team)) {
-	// 		throw new Error("User does not have necessary permissions");
-	// 	}
+		console.log(task)
+		
+		// this is half sudo code to be fixed
+		if (!user.project.contains(task.org) && !user.team.contains(task.team)) {
+			throw new Error("User does not have necessary permissions");
+		}
 
-	// 	next();
-	// } catch (error) {
-	// 	console.error(error);
-	// 	res.status(401).json({ message: error.message });
-	// }
+		next();
+	} catch (error) {
+		console.error(error);
+		res.status(401).json({ message: error.message });
+	}
 	next();
 };
