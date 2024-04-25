f1ebb477d574f497c83419a2f26db4715bb13362
Nick Jantz <thenickjantz@gmail.com>
2024-04-25 17:00:11 -0500

    add colors

--- app/components/pages/App.js
+++ app/components/pages/App.js
@@ -1,29 +1,10 @@
 import React from 'react';
 
-import {
-	Select,
-	SelectContent,
-	SelectItem,
-	SelectTrigger,
-	SelectValue,
-  } from "../ui/select"  
-
 import { PageWrapper } from '../utils/PageWrapper';
 
 export const App = () => {
 	return (
 		<PageWrapper>
-			<Select>
-			<SelectTrigger className="w-[180px]">
-				<SelectValue placeholder="Theme" />
-			</SelectTrigger>
-			<SelectContent>
-				<SelectItem value="light">Light</SelectItem>
-				<SelectItem value="dark">Dark</SelectItem>
-				<SelectItem value="system">System</SelectItem>
-			</SelectContent>
-			</Select>
-
 		</PageWrapper>
 	)
 }
